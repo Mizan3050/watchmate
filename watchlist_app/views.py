44a0a6beb2684daa082f901e78253e00667f536f
Meezan Shaikh <meezan@tradexa.co.in>
2024-06-18 08:56:11 +0530

    git initialized, project setup, app created

---
+++ watchlist_app/views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from watchlist_app.models import Movie
+
+# Create your views here.
+def movie_list(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies': list(movies.values())
+    }
+
+    return JsonResponse(data)
+    
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name': movie.name,
+        'active': movie.active,
+        'description': movie.description
+    }
+    return JsonResponse(data)

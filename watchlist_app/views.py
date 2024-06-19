e2a46f03c3ca9363771ddfae87aafc7f3cc47d08
Meezan Shaikh <meezan@tradexa.co.in>
2024-06-19 11:01:14 +0530

    crud for movies updated, function based views used for crud

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,21 +1,21 @@
-from django.http import JsonResponse
-from django.shortcuts import render
-from watchlist_app.models import Movie
+# from django.http import JsonResponse
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
 
-# Create your views here.
-def movie_list(request):
-    movies = Movie.objects.all()
-    data = {
-        'movies': list(movies.values())
-    }
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     data = {
+#         'movies': list(movies.values())
+#     }
 
-    return JsonResponse(data)
+#     return JsonResponse(data)
     
-def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    data = {
-        'name': movie.name,
-        'active': movie.active,
-        'description': movie.description
-    }
-    return JsonResponse(data)
+# def movie_detail(request, pk):
+#     movie = Movie.objects.get(pk=pk)
+#     data = {
+#         'name': movie.name,
+#         'active': movie.active,
+#         'description': movie.description
+#     }
+#     return JsonResponse(data)

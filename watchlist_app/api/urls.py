24041fc5737afc7ea3e7da79a5962c505154e150
Meezan Shaikh <meezan@tradexa.co.in>
2024-07-02 09:47:31 +0530

    review model added

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -4,6 +4,6 @@ from watchlist_app.api.views import WatchListDetailAV, WatchListListAV,StreamPla
 urlpatterns = [
     path('list', WatchListListAV.as_view(), name='movie_list'),
     path('<int:pk>', WatchListDetailAV.as_view(), name='movie_detail'),
-    path('stream',StreamPlatformListAV.as_view(), name = 'stream_list'),
-    path('stream/<int:pk>',StreamPlatformDetailAV.as_view(), name='stream_detail')
+    path('stream', StreamPlatformListAV.as_view(), name = 'stream_list'),
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream_detail')
 ]
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.fields import MaxLengthValidator, MaxValueValidator, MinLengthValidator, MinValueValidator
 
 # Create your models here.
 
@@ -10,7 +11,7 @@ class StreamPlatform(models.Model):
     def __str__(self):
         return self.name
 class WatchList(models.Model):
-    title  = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name = 'watchlist')
     active = models.BooleanField(default=True)
@@ -18,3 +19,14 @@ class WatchList(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='reviews')
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.rating) + ' ' + self.watchlist.title
